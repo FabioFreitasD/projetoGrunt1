ca245e998e1179ba0ee6b287edc0f8560e6a153a
Fabio Freitas Diniz <fabiofreitascmav@gmail.com>
2024-06-20 20:33:14 -0300

    Removendo pastas e adicionando ao .gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,6 +55,10 @@ module.exports = function(grunt) {
                         {
                             match: 'ENDERECO_DO_CSS',
                             replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
                         }
                     ]
                 },
@@ -79,7 +83,14 @@ module.exports = function(grunt) {
                 }
             }
         },
-        clean: ['prebuild']
+        clean: ['prebuild'],
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less'); //essa é a chamada do less para ser carregado nossos arquivos
@@ -87,7 +98,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-replace'); //pluguin para copiar os arquivos e mover para pastas criadas
     grunt.loadNpmTasks('grunt-contrib-htmlmin'); //pluguin para minificar o html
     grunt.loadNpmTasks('grunt-contrib-clean');  //pluguin para apagar pastas
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['watch']); //essa tarefa é a padrão que o grunt exige não precisanos mais referenciar a função com o final olaGrunt como é um array devido as [] podemos referenciar o less e o sass
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 }
